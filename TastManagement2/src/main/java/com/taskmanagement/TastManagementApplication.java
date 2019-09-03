37b8ae2013820019af3400190bfc85ba0c954a3e
CBT <nilesh.gupta6818@gmail.com>
2019-09-03 15:44:45 +0530

    TaskManagement 2

---
+++ TastManagement2/src/main/java/com/taskmanagement/TastManagementApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastManagementApplication.class, args);
+	}
+
+}
